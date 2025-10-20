28c8717d6e3ea1fdb19c111a80e5df064f0acac5
Anand Bhukya <bhukyaanandkumar6@gmail.com>
2025-10-21 00:14:01 +0530

    changes

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -5,13 +5,3 @@ variable "project_name" {
 variable "tags" {
   type = map(string)
 }
-variable "project_name" {
-  description = "Project name used as prefix for resource names"
-  type        = string
-}
-
-variable "tags" {
-  description = "Tags to be applied to all resources"
-  type        = map(string)
-  default     = {}
-}
