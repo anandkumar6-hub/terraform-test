68cf69af8363b0019e16dbb3dc356634e4033967
Anand Bhukya <bhukyaanandkumar6@gmail.com>
2025-10-20 16:02:23 +0530

    new project

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  value = module.ec2_instance.public_ip
-  description = "Public IP of the EC2 instance"
-}
-
-output "security_group_id" {
-  value = module.web_server_sg.security_group_id
-  description = "Security Group ID attached to EC2"
-}
