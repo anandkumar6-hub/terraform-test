dc1f7b68a347f4d92c8b8e189cc03f4c4a3c0001
Anand Bhukya <bhukyaanandkumar6@gmail.com>
2025-10-21 12:18:48 +0530

    changes

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "${var.project_name}-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # path to your local public key
+  public_key = "ubunutkey"  # path to your local public key
 }
 
 resource "aws_instance" "app" {
