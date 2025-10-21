4a65a0a9471f3823b28ed2420e5702e8ce85d398
Anand Bhukya <bhukyaanandkumar6@gmail.com>
2025-10-21 19:10:04 +0530

    changes

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
-  for_each           = toset(var.ec2_instance_ids)
-  alarm_name         = "${var.project_name}-ec2-cpu-${each.key}"
+  count               = length(var.ec2_instance_ids)
+  alarm_name          = "${var.project_name}-ec2-cpu-${count.index}"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 2
   metric_name         = "CPUUtilization"
@@ -9,27 +9,10 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   statistic           = "Average"
   threshold           = 70
   alarm_description   = "Alarm if CPU > 70% for 10 minutes"
-  dimensions = {
-    InstanceId = each.key
-  }
-
-  tags = merge(var.tags, { Name = "${var.project_name}-ec2-cpu" })
-}
-
-resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
-  count              = length(var.ec2_instance_ids)
-  alarm_name         = "CPUUtilization-${count.index}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 120
-  statistic           = "Average"
-  threshold           = 80
-  alarm_description   = "Alarm when CPU exceeds 80%"
   alarm_actions       = []
   dimensions = {
     InstanceId = var.ec2_instance_ids[count.index]
   }
-}
 
+  tags = merge(var.tags, { Name = "${var.project_name}-ec2-cpu-${count.index}" })
+}
