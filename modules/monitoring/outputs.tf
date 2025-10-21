1b65188e619881e793ec61ab92c72cbc3d806fd4
Anand Bhukya <bhukyaanandkumar6@gmail.com>
2025-10-21 19:19:44 +0530

    changes

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -1,11 +1,3 @@
 output "ec2_cpu_alarms" {
   value = [for alarm in aws_cloudwatch_metric_alarm.ec2_cpu : alarm.arn]
 }
-
-output "rds_cpu_alarm" {
-  value = aws_cloudwatch_metric_alarm.rds_cpu.arn
-}
-
-output "alb_5xx_alarm" {
-  value = aws_cloudwatch_metric_alarm.alb_5xx.arn
-}
